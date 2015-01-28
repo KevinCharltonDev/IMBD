dbe58e8d02ff8292d9dc9e79141ce79d6808e9fd
Jason Toomey <jrtoomey12@gmail.com>
2015-01-28 12:44:48 -0500

    Home page and default css

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>IMBD</title>
+<link href="css/default.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<header>
+	<div class="left">
+	Home
+	</div>
+	<div class="right">
+	Sign in
+	</div>
+</header>
+<article>
+	<h1>Indiana Music Business Directory</h1>
+	<section>
+	<h2>Title for text below</h2>
+	<p>Some text will go here. Some text will go here. Some text will go here. Some text will go here. Some text will go here. Some text will go here. Some text will go here. Some text will go here. Some text will go here. Some text will go here. Some text will go here. Some text will go here. Some text will go here. Some text will go here. Some text will go here. Some text will go here. Some text will go here. Some text will go here. </p>
+	</section>
+	<section>
+	<h2>Search</h2>
+	<form action="" method="GET">
+		<input type="text" name="search">
+	</form>
+	</section>
+</article>
+<footer>
+This is the footer.
+</footer>
+</body>
+</html>
