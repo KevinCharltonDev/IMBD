a0331d750427e230520927c779f282ca85064d31
Jason Toomey <jrtoomey12@gmail.com>
2015-02-04 15:37:35 -0500

    Added php file to connect to database

---
+++ connect/config.php
@@ -0,0 +1,13 @@
+<?php
+$server_name = 'localhost';
+
+$normal_user = 'imbd_normal';
+$student_user = 'imbd_student';
+$moderator_user = 'imbd_moderator';
+
+$normal_password = 'YB23Kdb3YmFLCtb';
+$student_password = 'vb3DcZvAv6xmiZ4';
+$moderator_password = 'Me4WpVKLAd7QWrh';
+
+$database_name = 'imbd_test';
+?>
