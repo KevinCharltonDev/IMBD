141c941327b2aac2c29562026b95910f3ea2a9c8
Kevin <krcharltongu@bsu.edu>
2015-04-17 14:45:03 -0400

    Added about message to homepage as requested by client.

--- index.php
+++ index.php
@@ -22,6 +22,10 @@ if(!isset($_SESSION['Email'])) {
 <section>
 <h2>Search</h2>
 <div class="content">
+<p>The Indiana Music Business Directory began in 2014 as a project by Robert Willey and his music industry students in the School of Music at Ball State University <a href=http://tinyurl.com/indianamusic>tinyurl.com/indianamusic</a> in order to help develop the music business infrastructure in Indiana and help clients connect with providers.
+</p>
+<p>In 2015 Jason Toomey and Kevin Charlton created this web-based app to make it easier to add and access information from a desktop computer or mobile device. The work was done as a capstone project under the direction of Willey and Lan Lin, their CS495 professor.
+</p>
 <form action="search.php" method="GET" id="searchForm">
 <input type="text" name="search">
 <input type="submit" value="Search">
